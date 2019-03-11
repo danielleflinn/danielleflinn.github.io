e48b8019a916f636c6b77dcb57e5130e1275954a
Danielle <danielleflinn29@gmail.com>
2019-03-11 11:45:04 -0700

    cutomized light gallery

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,11 @@ console.log('OK');
 // Light Gallery
 
 $('.gallery-1').lightGallery({
-    selector: 'a'
+    selector: 'a',
+    thumbnail: false,
+    download: false,
+    autoplay: false,
+    share: false,
+    autoplayControls: false,
+    mode: 'lg-scale-up'
 });
