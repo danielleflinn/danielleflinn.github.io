e67035bba382327e9e766c4f1edfbfb28f8c767a
Danielle Flinn <danielleflinn29@gmail.com>
2019-03-12 08:32:41 -0700

    added behavior
    from codepen

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 // Everything OK?
 console.log('OK');
 
+//heder animation
+
+$('.arrow-down').click(function() {
+  
+  $('.logo-wrapper').addClass('enter-site');
+});
+
 // Light Gallery
 
 $('.gallery-1').lightGallery({
@@ -12,3 +19,12 @@ $('.gallery-1').lightGallery({
     autoplayControls: false,
     mode: 'lg-scale-up'
 });
+
+// footer date
+
+var today = new Date();
+var year = today.getFullYear();
+
+console.log(today);
+
+$('.year').text(year);
